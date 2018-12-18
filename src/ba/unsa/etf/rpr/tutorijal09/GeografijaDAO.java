ee03e637156495a6cd0665cf0627b88a08c25db1
mkavcic <mkavcic1@etf.unsa.ba>
2018-12-18 18:00:43 +0100

    Deklaracija metoda u klasi GeografijaDAO

--- src/ba/unsa/etf/rpr/tutorijal09/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/tutorijal09/GeografijaDAO.java
@@ -1,4 +1,45 @@
 package ba.unsa.etf.rpr.tutorijal09;
 
+import java.util.ArrayList;
+
 public class GeografijaDAO {
+
+
+    Grad GlavniGrad(String drzava){
+        Grad g=new Grad();
+        return g;
+    }
+
+    void obrisiDrzavu(String drzava){}
+
+    ArrayList<Grad> gradovi(){
+        ArrayList<Grad> a=new ArrayList<>();
+        return a;
+    }
+
+    void dodajGrad(Grad grad){
+
+    }
+
+    void dodajDrzavu(Drzava drzava){
+
+    }
+
+    void izmijeniGrad(Grad grad){
+
+    }
+
+    Drzava nadjiDrzavu(){
+        Drzava d=new Drzava();
+        return d;
+    }
+
+    String ispisiGradove(){
+        String c="";
+        return c;
+    }
+
+    void glavniGrad(){
+
+    }
 }
