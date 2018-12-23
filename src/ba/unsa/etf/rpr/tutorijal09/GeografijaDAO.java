ae15121348db3a965cffb7979ab05726bd4d3625
mkavcic <mkavcic1@etf.unsa.ba>
2018-12-23 12:22:57 +0100

    GeografijaDAO reform.kod

--- src/ba/unsa/etf/rpr/tutorijal09/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/tutorijal09/GeografijaDAO.java
@@ -10,21 +10,21 @@ import static ba.unsa.etf.rpr.tutorijal09.Main.createNewDatabase;
 import static ba.unsa.etf.rpr.tutorijal09.Main.createNewTable;
 
 public class GeografijaDAO {
-    ArrayList<Grad> gradovi=new ArrayList<>();
-    ArrayList<Drzava> drzave=new ArrayList<>();
-    private static GeografijaDAO instanca=null;
+    ArrayList<Grad> gradovi = new ArrayList<>();
+    ArrayList<Drzava> drzave = new ArrayList<>();
+    private static GeografijaDAO instanca = null;
     private static Connection conn;
     public String url = "jdbc:sqlite/baza.db";
     private static PreparedStatement stmt;
 
 
     public static GeografijaDAO getInstance() {
-        if(instanca==null) initialize();
+        if (instanca == null) initialize();
         return instanca;
     }
 
-    private  static void initialize() {
-        if(instanca==null){
+    private static void initialize() {
+        if (instanca == null) {
             createNewDatabase("baza");
             createNewTable();
             insertToDatabase();
@@ -32,7 +32,7 @@ public class GeografijaDAO {
         instanca = new GeografijaDAO();
     }
 
-    public static  void insertToDatabase(){
+    public static void insertToDatabase() {
         connect();
         try {
             stmt = conn.prepareStatement("INSERT INTO drzava(id,naziv,glavni_grad) VALUES(1,'Velika Britanija',1)");
@@ -52,65 +52,63 @@ public class GeografijaDAO {
         }
     }
 
-    public static void connect(){  // source : https://www.codejava.net/java-se/jdbc/connect-to-a-database-with-jdbc
-        try{
+    public static void connect() {  // source : https://www.codejava.net/java-se/jdbc/connect-to-a-database-with-jdbc
+        try {
             String url = "jdbc:sqlite:baza.db";
             conn = DriverManager.getConnection(url);
-            if (conn!=null)
+            if (conn != null)
                 System.out.println("Spojeno.");
-        }
-        catch (SQLException ex) {
-            System.err.println("Spajanje nije uspjelo."+ex.getMessage());
+        } catch (SQLException ex) {
+            System.err.println("Spajanje nije uspjelo." + ex.getMessage());
         }
     }
 
-    Grad GlavniGrad(String drzava){
-        Grad g=new Grad();
+    Grad GlavniGrad(String drzava) {
+        Grad g = new Grad();
         return g;
     }
 
-    public void obrisiDrzavu(String drzava){
-        for(var x: drzave){
-            if(x.getNaziv().equals(drzava)) drzave.remove(x);
+    public void obrisiDrzavu(String drzava) {
+        for (var x : drzave) {
+            if (x.getNaziv().equals(drzava)) drzave.remove(x);
         }
     }
 
-    ArrayList<Grad> gradovi(){
-        ArrayList<Grad> a=new ArrayList<>();
+    ArrayList<Grad> gradovi() {
+        ArrayList<Grad> a = new ArrayList<>();
         return a;
     }
 
-    void dodajGrad(Grad grad){
+    void dodajGrad(Grad grad) {
         gradovi.add(grad);
     }
 
-    void dodajDrzavu(Drzava drzava){
+    void dodajDrzavu(Drzava drzava) {
         drzave.add(drzava);
     }
 
-    void izmijeniGrad(Grad grad){
-        try{
-            stmt=conn.prepareStatement("UPDATE main.drzava SET main.dzava.naziv=?, main.drzava.glavniGrad=?, main.drzava.brojStanovnika=?");
+    void izmijeniGrad(Grad grad) {
+        try {
+            stmt = conn.prepareStatement("UPDATE main.drzava SET main.dzava.naziv=?, main.drzava.glavniGrad=?, main.drzava.brojStanovnika=?");
             stmt.setInt(1, grad.getId());
             stmt.setString(2, grad.getNaziv());
             stmt.setInt(3, grad.getBrojStanovnika());
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    Drzava nadjiDrzavu(){
-        Drzava d=new Drzava();
-        for(var x: drzave){
-            if(x.getNaziv().equals(x)) d=x;
+    Drzava nadjiDrzavu() {
+        Drzava d = new Drzava();
+        for (var x : drzave) {
+            if (x.getNaziv().equals(x)) d = x;
         }
         return d;
     }
 
-    public Grad glavniGrad(String drzava){
-        for(var x: drzave){
-            if(x.getNaziv().equals(drzava)) return x.getGlavniGrad();
+    public Grad glavniGrad(String drzava) {
+        for (var x : drzave) {
+            if (x.getNaziv().equals(drzava)) return x.getGlavniGrad();
         }
         return null;
     }
