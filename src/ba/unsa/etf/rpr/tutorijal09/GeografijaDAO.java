b8aafbd8121f3e953e74f82c10b4691159b80a4b
mkavcic <mkavcic1@etf.unsa.ba>
2018-12-23 12:21:51 +0100

    GeografijaDAO prepravljeno

--- src/ba/unsa/etf/rpr/tutorijal09/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/tutorijal09/GeografijaDAO.java
@@ -13,11 +13,9 @@ public class GeografijaDAO {
     ArrayList<Grad> gradovi=new ArrayList<>();
     ArrayList<Drzava> drzave=new ArrayList<>();
     private static GeografijaDAO instanca=null;
-    private static final String INSERT_SQL1 = "INSERT INTO Remuneraciones(id, naziv, , glavni_grad) VALUES(?, ?, ?)";
-    private static final String INSERT_SQL2 = "INSERT INTO Remuneraciones(id, naziv, , broj_stanovnika, drzava) VALUES(?, ?, ?,?)";
-    private static Connection conn;  /* i ostalo što treba za bazu */
+    private static Connection conn;
     public String url = "jdbc:sqlite/baza.db";
-    private static PreparedStatement stmt1, stmt2;
+    private static PreparedStatement stmt;
 
 
     public static GeografijaDAO getInstance() {
@@ -37,17 +35,17 @@ public class GeografijaDAO {
     public static  void insertToDatabase(){
         connect();
         try {
-            stmt1 = conn.prepareStatement("INSERT INTO drzava(id,naziv,glavni_grad) VALUES(1,'Velika Britanija',1)");
-            stmt1.execute();
-            stmt1 = conn.prepareStatement("INSERT INTO drzava(naziv,glavni_grad) VALUES(2,'Francuska',2)");
-            stmt1.execute();
-            stmt1 = conn.prepareStatement("INSERT INTO drzava(naziv,glavni_grad) VALUES(3,'Austrija',3)");
-            stmt1.execute();
-            stmt1 = conn.prepareStatement("INSERT INTO drzava(naziv,glavni_grad) VALUES(4,'Velika Britanija',4)");
-            stmt1.execute();
-            stmt1 = conn.prepareStatement("INSERT INTO drzava(naziv,glavni_grad) VALUES(5,'Austrija',5)");
-            stmt1.execute();
-            stmt1.close();
+            stmt = conn.prepareStatement("INSERT INTO drzava(id,naziv,glavni_grad) VALUES(1,'Velika Britanija',1)");
+            stmt.execute();
+            stmt = conn.prepareStatement("INSERT INTO drzava(naziv,glavni_grad) VALUES(2,'Francuska',2)");
+            stmt.execute();
+            stmt = conn.prepareStatement("INSERT INTO drzava(naziv,glavni_grad) VALUES(3,'Austrija',3)");
+            stmt.execute();
+            stmt = conn.prepareStatement("INSERT INTO drzava(naziv,glavni_grad) VALUES(4,'Velika Britanija',4)");
+            stmt.execute();
+            stmt = conn.prepareStatement("INSERT INTO drzava(naziv,glavni_grad) VALUES(5,'Austrija',5)");
+            stmt.execute();
+            stmt.close();
 
         } catch (Exception e) {
             e.getMessage();
@@ -92,10 +90,10 @@ public class GeografijaDAO {
 
     void izmijeniGrad(Grad grad){
         try{
-            stmt1=conn.prepareStatement("UPDATE main.drzava SET main.dzava.naziv=?, main.drzava.glavniGrad=?, main.drzava.brojStanovnika=?");
-            stmt1.setInt(1, grad.getId());
-            stmt1.setString(2, grad.getNaziv());
-            stmt1.setInt(3, grad.getBrojStanovnika());
+            stmt=conn.prepareStatement("UPDATE main.drzava SET main.dzava.naziv=?, main.drzava.glavniGrad=?, main.drzava.brojStanovnika=?");
+            stmt.setInt(1, grad.getId());
+            stmt.setString(2, grad.getNaziv());
+            stmt.setInt(3, grad.getBrojStanovnika());
         }
         catch(Exception e){
             e.printStackTrace();
@@ -108,11 +106,6 @@ public class GeografijaDAO {
             if(x.getNaziv().equals(x)) d=x;
         }
         return d;
-    }
-
-    String ispisiGradove(){
-        String c="";
-        return c;
     }
 
     public Grad glavniGrad(String drzava){
